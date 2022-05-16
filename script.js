72ca0526b13a6ece2a722c0e008aad4593504e4d
Asadbek <a.nosirjonov@uic.group>
2022-05-17 01:07:28 +0500

    feat: functions

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const block = document.querySelectorAll('.block'),
-        wrapper = document.querySelector('.wrapper'),
-        field = document.querySelector('.inner');
+	const block = document.querySelectorAll('.block'),
+		wrapper = document.querySelector('.wrapper'),
+		field = document.querySelector('.wrapper__inner'),
+		prev = document.querySelector('.prev'),
+		next = document.querySelector('.next'),
+		current = document.querySelector('#current'),
+		total = document.querySelector('#total');
 
-    let width = getComputedStyle(wrapper).width
-    field.style.width = 100 * block.length + '%';
+	let index = 1;
+	let offset = 0;
+	let width = getComputedStyle(wrapper).width;
+
+	field.style.width = 100 * block.length + '%';
+	field.style.display = 'flex';
+	field.style.transition = '0.5s all';
+	wrapper.style.overflow = 'hidden';
+
+	block.forEach(slide => {
+		slide.style.width = width;
+	})
+
+	next.addEventListener('click', () => {
+		if(offset == (+width.slice(0, width.length - 2) * (block.length - 1))) {
+			offset = 0;
+		}else{
+			offset += +width.slice(0, width.length - 2)
+		}
+		field.style.transform = `translateX(-${offset}px)`
+	})
+
+	prev.addEventListener('click', () => {
+		if(offset == 0) {
+			offset = (+width.slice(0, width.length - 2) * (block.length - 1))
+		}else{
+			offset -= +width.slice(0, width.length - 2)
+		}
+		field.style.transform = `translateX(${offset}px)`
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
 
 // `
